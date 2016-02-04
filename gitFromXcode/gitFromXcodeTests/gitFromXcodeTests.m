33fa917cf1112e5ab197b17079cfb38775794666
Nicolas Matias Di Giacomo <nicolasmatiasdigiacomo@nicolass-mac-mini.local>
2016-02-04 12:34:39 -0300

    a

---
+++ gitFromXcode/gitFromXcodeTests/gitFromXcodeTests.m
@@ -0,0 +1,39 @@
+//
+//  gitFromXcodeTests.m
+//  gitFromXcodeTests
+//
+//  Created by Nicolas Matias Di Giacomo  on 2/4/16.
+//  Copyright © 2016 Nicolas Matias Di Giacomo . All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface gitFromXcodeTests : XCTestCase
+
+@end
+
+@implementation gitFromXcodeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
